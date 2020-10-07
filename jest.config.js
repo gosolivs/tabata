5dce351250894008d49e054e16ad67c0c0b0cf7e
Ivan Solovev <ivan@solovev.one>
2020-10-07 22:44:04 +0300

    Clean up jest config

--- jest.config.js
+++ jest.config.js
@@ -6,22 +6,6 @@ module.exports = {
 		"^.+\\.jsx?$": require.resolve("ts-jest"),
 	},
 
-	// moduleFileExtensions: ["js", "json", "vue"],
-	// collectCoverage: true,
-	//
-	// transform: {
-	// 	"^.+\\.vue$": "jest-vue-preprocessor",
-	// 	".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
-	// 		"jest-transform-stub",
-	// 	"^.+\\.js$": "babel-jest",
-	// },
-	//
-	// moduleNameMapper: {
-	// 	"^@/(.*)$": "<rootDir>/src/$1",
-	// },
-	//
-	// snapshotSerializers: ["jest-serializer-vue"],
-
 	testMatch: [
 		"**/tests/unit/**/*.spec.js",
 		"**/__tests__/*.spec.js",
